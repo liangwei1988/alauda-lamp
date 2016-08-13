80564a29d7dbeafed13db4b66c14637ef8ce07c9
liangwei <362253310@qq.com>
2016-08-14 03:34:07 +0800

    apache 增加ssi

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@
 sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
     -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php5/apache2/php.ini
 
+ln -s /etc/apache2/mods-available/include.load /etc/apache2/mods-enabled
+
 exec supervisord -n
